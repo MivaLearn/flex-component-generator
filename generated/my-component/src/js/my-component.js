d0587210b2b6e899df59461f9a1badc72be4616c
MivaLearn <nakins@mivamerchant.com>
2025-04-23 08:37:44 -0700

    removed files in generated folder

--- generated/my-component/src/js/my-component.js
+++
@@ -1,6 +0,0 @@
-// Script for My Component (my-component)
-
-document.addEventListener('DOMContentLoaded', () => {
-	// Add component script logic here
-	console.log('Component my-component script loaded.');
-});
